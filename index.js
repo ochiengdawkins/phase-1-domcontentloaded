395542d7e819cdd9a4fb5e959333b7ee85fcc80a
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 14:26:59 +0300

    commit tree 9e727b99f5b3892176965baccb57b18ec9cf771b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+console.log(document.querySelector("p").textContent)
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").textContent = "This is really cool!"
+    console.log(document.getElementById("text").textContent)
+})
